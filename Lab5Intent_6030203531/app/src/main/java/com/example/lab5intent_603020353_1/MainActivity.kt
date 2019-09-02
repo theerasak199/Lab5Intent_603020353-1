f5f9b1dad40d296f067c29cbe7f6fb0e6da3c55c
theerasak199 <54529697+theerasak199@users.noreply.github.com>
2019-09-02 17:26:09 +0700

    Lab5Intent_603020353-1
    2/9/2019

---
+++ Lab5Intent_6030203531/app/src/main/java/com/example/lab5intent_603020353_1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lab5intent_603020353_1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickShowDetail(v: View) {
+        val intent = Intent ( this, SecondActivity::class.java)
+        intent.putExtra("stdData", Student(IdEDT.text.toString(),nameEDT.text.toString(),
+            ageEDT.text.toString().toInt())
+        )
+        startActivity(intent)
+    }
+}
